10c5895fd68396c21cadee2776f8eb92c8d47fb2
Kirill Yakovenko <kirill.yakovenko@me.com>
2019-03-09 14:59:40 +0200

    Update path

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const { discover } = require('./discover');
-const { connect } = require('./connect');
-const commands = require('./commands');
-
-module.exports = {
-  discover,
-  connect,
-  commands
-}
